c3e9beae25c91682b5d78c81e1d77079cb1f2d02
taisei.sumiyama <80329420+onakama@users.noreply.github.com>
2024-09-06 12:03:14 +0900

    name修正

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
-            baseName = "shared"
+            baseName = "gmpproject"
             binaryOption("bundleId", "org.example.${xcFrameworkName}")
             xcf.add(this)
             isStatic = true
