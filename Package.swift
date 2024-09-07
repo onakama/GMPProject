c7e651f946aaed9115ed8c650a03fdad512f7de7
taisei.sumiyama <80329420+onakama@users.noreply.github.com>
2024-09-07 22:42:53 +0900

    path修正

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Shared",
-         path: "shared/build/XCFrameworks/release"
+         path: "shared/build/XCFrameworks/release/Shared.xcframework"
          )
 
    ]
