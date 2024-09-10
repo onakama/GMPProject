f3fc2936e0fdac97d995b0bc4771ab67c0f08884
taisei.sumiyama <80329420+onakama@users.noreply.github.com>
2024-09-10 15:34:20 +0900

    skie追加

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("co.touchlab.skie") version "0.8.4"
 }
 
 kotlin {
